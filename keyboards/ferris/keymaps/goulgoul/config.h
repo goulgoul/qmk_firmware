2430ca5609b56989168d34396ebf09518460d45a
Antoine Goulin <antoine.goulin@ecole2600.com>
2025-09-03 22:35:30 +0200

    Add Flow-Tap to prevent unwanted mod-tap trigger while rolling fingers

--- keyboards/ferris/keymaps/goulgoul/config.h
+++ keyboards/ferris/keymaps/goulgoul/config.h
@@ -27,8 +27,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 100
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 180
-#define COMBO_TERM 50
+#define TAPPING_TERM 200
+#define FLOW_TAP_TERM 100
+#define COMBO_TERM 30
 /*#define PERMISSIVE_HOLD*/
 #define QUICK_TAP_TERM 0
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
