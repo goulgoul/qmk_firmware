c866762bc265debfe6282484692925f6da7d3afe
agoulin <antoine.goulin@cs-soprasteria.com>
2025-09-08 14:46:09 +0200

    add back ralt on letter i

--- keyboards/ferris/keymaps/goulgoul/keymap.h
+++ keyboards/ferris/keymaps/goulgoul/keymap.h
@@ -21,7 +21,7 @@
 const uint16_t PROGMEM comboWF[]    = { KC_W, KC_F,       COMBO_END };
 const uint16_t PROGMEM comboRS[]    = { HM_R, HM_S,       COMBO_END };
 const uint16_t PROGMEM comboUY[]    = { KC_U, KC_Y,       COMBO_END };
-const uint16_t PROGMEM comboEI[]    = { HM_E, KC_I,       COMBO_END };
+const uint16_t PROGMEM comboEI[]    = { HM_E, HM_I,       COMBO_END };
 
 const uint16_t PROGMEM comboNU[]    = { HM_N, KC_U,        COMBO_END };
 const uint16_t PROGMEM comboLU[]    = { KC_L, KC_U,       COMBO_END };
